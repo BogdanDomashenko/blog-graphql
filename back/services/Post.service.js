ce0fba87d9607efb5516a1449baee79bcca9a382
BogdanDomashenko <got.bogdan@gmail.com>
2022-09-19 15:00:40 +0300

    upd

--- back/services/Post.service.js
+++ back/services/Post.service.js
@@ -3,7 +3,7 @@ const { Post } = require("../models/models");
 exports.PostService = {
   async create(userId, title, content) {
     const post = new Post({ title, content, author: userId });
-    return post.save();
+    return (await post.save()).populate("author");
   },
   async getAll() {
     const posts = await Post.find().populate("author");
